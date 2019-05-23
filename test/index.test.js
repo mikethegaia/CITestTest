ec74d6555e5e41d100dd84ec33cef1d1ce9bf904
MikeTheGaia <mikkou95@gmail.com>
2019-05-23 11:09:34 -0500

    Circle CI config

--- test/index.test.js
+++ test/index.test.js
@@ -1,6 +1,10 @@
 const sum = require('../index');
 
-test('adds 1 + 2 to equal 3', sumTest);
+test('adds 1 + 2 to equals 3', sumTest);
+
+test('adds 2 + 5 equals 7', () => {
+    expect(sum(2, 5)).toBe(7);
+});
 
 function sumTest() {
     expect(sum(1, 2)).toBe(3);
